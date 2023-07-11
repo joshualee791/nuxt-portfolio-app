a09a3327c4038e5f7317a89e87e385e54bd54e75
Joshua <120612998+joshualee791@users.noreply.github.com>
2023-07-11 02:05:57 -0500

    Play with Tailwind on Header comp

--- components/Header.vue
+++ components/Header.vue
@@ -1,30 +1,49 @@
 <template>
-  <header class="bg-gray-900 text-white py-4 px-6">
+  <header class="bg-transparent text-gray-900 py-4 px-64">
     <nav class="flex justify-between items-center">
       <div class="flex items-center">
-        <h1 class="text-2xl font-bold">My Website</h1>
+        <img class="mr-8 h-10" src="/assets/logo1.png" alt="Logo" />
+        <h1 class="text-2xl font-black">
+          <router-link to="/" :class="{ 'text-black': $route.path === '/' }">
+            joshualeegarza
+          </router-link>
+        </h1>
       </div>
       <div class="flex items-center">
         <router-link
           to="/"
-          class="mr-4 py-2 px-3 rounded-lg hover:bg-gray-800"
-          :class="{ 'bg-gray-800': $route.path === '/' }"
+          class="text-black hover:font-semibold mx-3"
+          :class="{ 'text-black': $route.path === '/' }"
         >
           Home
         </router-link>
         <router-link
           to="/about"
-          class="mr-4 py-2 px-3 rounded-lg hover:bg-gray-800"
-          :class="{ 'bg-gray-800': $route.path === '/about' }"
+          class="text-black font-semibold hover:text-teal-700 mx-3"
+          :class="{ 'text-black': $route.path === '/about' }"
         >
           About
         </router-link>
         <router-link
-          to="/contact"
-          class="py-2 px-3 rounded-lg hover:bg-gray-800"
-          :class="{ 'bg-gray-800': $route.path === '/contact' }"
+          to="/"
+          class="text-black font-semibold mx-3"
+          :class="{ 'text-black': $route.path === '/' }"
         >
-          Contact
+          Home
+        </router-link>
+        <router-link
+          to="/"
+          class="text-black hover:text-teal-700 mx-3"
+          :class="{ 'text-teal-700': $route.path === '/' }"
+        >
+          Home
+        </router-link>
+        <router-link
+          to="/"
+          class="text-black hover:font-semibold mx-2"
+          :class="{ 'text-black': $route.path === '/' }"
+        >
+          Home
         </router-link>
       </div>
     </nav>
--- pages/about.vue
+++ pages/about.vue
@@ -3,6 +3,6 @@ const route = useRoute();
 </script>
 <template>
   <div class="my-12 px-64">
-    <h1>Nuxt Routing set up successfully!</h1>
+    <h1>About Me</h1>
   </div>
 </template>
